3d7517c3dd1e595501e00b3e4bf8bec24eb9d6e6
Mark Tilbrook <marktilbrook2906@gmail.com>
2018-03-09 17:48:27 +0000

    Added sprite /w texture
    Sprite jumps around screen and bounces off walls!

--- SFML Game/src/Main.cpp
+++ SFML Game/src/Main.cpp
@@ -5,9 +5,18 @@ int main(int argc, char** argv[]) {
 	//window constructor
 	sf::RenderWindow window(sf::VideoMode(640, 480),"Game Window!");
 
-	sf::RectangleShape rectangle(sf::Vector2f(120.f, 128.f));
-	rectangle.setFillColor(sf::Color::Cyan);
-	rectangle.setPosition(320, 240);
+	sf::Texture texture;
+	if (!texture.loadFromFile("res/mushroom1.png"))
+	{
+		//handle loading error
+	}
+
+	sf::Sprite mushroom(texture);
+
+	sf::Vector2u size = texture.getSize();
+	mushroom.setOrigin(size.x / 2, size.y / 2);
+	sf::Vector2f increment(0.4f, 0.4f);
+
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -17,9 +26,27 @@ int main(int argc, char** argv[]) {
 				window.close();
 			}
 		}
+		
+		if ((mushroom.getPosition().x + (size.x / 2) >
+			window.getSize().x && increment.x > 0) ||
+			(mushroom.getPosition().x - (size.x / 2) < 0 &&
+				increment.x < 0))
+		{
+			// Reverse the direction on X axis.
+			increment.x = -increment.x;
+		}
+		if ((mushroom.getPosition().y + (size.y / 2) >
+			window.getSize().y && increment.y > 0) ||
+			(mushroom.getPosition().y - (size.y / 2) < 0 &&
+				increment.y < 0))
+		{
+			increment.y = -increment.y;
+		}
+
+		mushroom.setPosition(mushroom.getPosition() + increment);
 		window.clear(sf::Color::Black);
 		// Draw here.
-		window.draw(rectangle);
+		window.draw(mushroom);
 		window.display();
 	}
 }
